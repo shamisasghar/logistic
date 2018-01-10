e2a0542031daec9126eb7d9e90ddb8151c511ec1
Qasim Ali <qasimali0001@outlook.com>
2018-01-10 22:44:57 +0500

    One Signal Added

--- app/src/main/java/com/logistics/hypernym/logistic/SplashActivity.java
+++ app/src/main/java/com/logistics/hypernym/logistic/SplashActivity.java
@@ -5,9 +5,12 @@ import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.logistics.hypernym.logistic.R;
 import com.logistics.hypernym.logistic.utils.ActivityUtils;
+import com.logistics.hypernym.logistic.utils.ScheduleUtils;
+import com.onesignal.OneSignal;
 
 /**
  * Created by shamis on 08-Dec-17.
@@ -27,6 +30,7 @@ public class SplashActivity extends AppCompatActivity implements ActivityCompat.
         setContentView(R.layout.activity_splash);
         mHandler = new Handler();
         mHandler.postDelayed(mRunnable, 2000);
+        initOneSignal();
 
     }
 
@@ -36,6 +40,25 @@ public class SplashActivity extends AppCompatActivity implements ActivityCompat.
             mHandler.removeCallbacks(mRunnable);
         }
         super.onBackPressed();
+    }
+
+    public void initOneSignal() {
+        // Logging set to help debug issues, remove before releasing your app.
+//        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.WARN);
+//
+        OneSignal.startInit(this)
+                .init();
+
+        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+            @Override
+            public void idsAvailable(String userId, String registrationId) {
+                Toast.makeText(SplashActivity.this, userId, Toast.LENGTH_SHORT).show();
+                ScheduleUtils.saveUserOneSignalId(getApplicationContext(), userId);
+                Log.e("debug", "User:" + userId);
+                if (registrationId != null)
+                    Log.e("debug", "registrationId:" + registrationId);
+            }
+        });
     }
 
 }
